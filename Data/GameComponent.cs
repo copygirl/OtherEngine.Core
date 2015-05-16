24704e4be9a3d1b11316405790d4f2ef9a529f57
copygirl <halfnerd.copygirl@gmail.com>
2015-05-16 06:05:24 +0200

    Add static utility functions to GameComponent

--- Data/GameComponent.cs
+++ Data/GameComponent.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 using OtherEngine.Core.Events;
 
 namespace OtherEngine.Core.Data
@@ -20,12 +21,43 @@ namespace OtherEngine.Core.Data
 		/// <summary>
 		/// Gets the "friendly" name of this GameComponent, without the "Component" suffix.
 		/// </summary>
-		public string Name { get { return _matchTrailingComponent.Replace(GetType().Name, ""); } }
+		public string Name { get { return GetName(GetType()); } }
+
+		#region ToString / name related
+
+		static string ToString(string name)
+		{
+			return string.Format("[Component: {0}]", name);
+		}
 
 		public sealed override string ToString()
 		{
-			return string.Format("[Component: {0}]", Name);
+			return ToString(Name);
 		}
+
+		/// <summary>
+		/// Returns the name of a GameComponent type, same as the instance version.
+		/// </summary>
+		public static string GetName(Type componentType)
+		{
+			if (componentType == null)
+				throw new ArgumentNullException("componentType");
+			if (!componentType.IsSubclassOf(typeof(GameComponent)))
+				throw new ArgumentException(String.Format(
+					"{0} is not a GameComponent", componentType), "componentType");
+
+			return _matchTrailingComponent.Replace(componentType.Name, "");
+		}
+
+		/// <summary>
+		/// Returns a string representation of the GameComponent type, same as the instance version.
+		/// </summary>
+		public static string ToString(Type componentType)
+		{
+			return ToString(GetName(componentType));
+		}
+
+		#endregion
 	}
 }
 
