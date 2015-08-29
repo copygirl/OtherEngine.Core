9e968798bd4465781d3ee31e42e57ccc0fef29e8
copygirl <halfnerd.copygirl@gmail.com>
2015-08-29 02:34:53 +0200

    Move core module loading to Game constructor

--- Game.cs
+++ Game.cs
@@ -19,6 +19,8 @@ namespace OtherEngine.Core
 			Controllers = new ControllerManager(this);
 			Events = new EventManager(this);
 			Modules = new ModuleManager(this);
+
+			Modules.Load(typeof(Game).Assembly);
 		}
 	}
 }
